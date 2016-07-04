98f2b8d81d7c20b4fd6f69b3ed9c3a69c4640af9
Rohit Garg <rohitgarg@Rohits-MacBook-Pro-2.local>
2016-07-04 16:30:59 +0530

    adding level parsing

---
+++ Assets/Game.cs
@@ -0,0 +1,26 @@
+using System.Xml;
+using System.Xml.Serialization;
+using System.Collections.Generic;
+using System.Collections;
+
+public class Game {
+	public Board board;
+
+	public class Board {
+		public int width;
+		public int height;
+
+		public ArrayList rows;
+	}
+
+	public class Row {
+
+		public ArrayList blocks;
+	}
+
+	public class Block {
+		public int type;
+		public bool isDestination;
+		public bool isSource;
+	}
+}
